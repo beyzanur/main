e7d3165958a51182ed45bf40c469a995d58607bb
beyzanur <beyzanurkocak@std.sehir.edu.tr>
2015-10-22 09:24:53 +0300

    beyza function

--- main/PMi.py
+++ main/PMi.py
@@ -3,7 +3,6 @@ __author__ = 'beyzanurkocak'
 def size_list(n):
     return len(n)
 def summing(a,b):
-    retur
-    n a+b
-def double(a):
-    return a*2
+    return a+b
+def beyza(a):
+    return a
